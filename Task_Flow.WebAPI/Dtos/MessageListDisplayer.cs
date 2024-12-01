04ab6d5178369b4e4b6f55637a40fb8f98318ef3
Sevgi <sevgi.elesgerova@gmail.com>
2024-12-01 10:22:15 +0400

    messagelist

---
+++ Task_Flow.WebAPI/Dtos/MessageListDisplayer.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_Flow.WebAPI.Dtos
+{
+    public class MessageListDisplayer
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; } 
+        public string SentDate { get; set; }
+        public string Image {  get; set; }
+
+
+    }
+}
